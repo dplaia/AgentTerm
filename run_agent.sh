7ecdd3ca1fc2783541f8365ab4cfc94c6826aa5d
dplaia <davide.plaia@gmail.com>
2025-01-21 14:41:26 +0100

    run_agent.sh removed.

--- run_agent.sh
+++
@@ -1,8 +0,0 @@
-# Navigate to the project directory
-cd /home/dplaia/Projekte/AgentTerm
-
-# Activate the virtual environment (adjust the path as needed)
-#source .venv/bin/activate
-
-# Run the script
-uv run agent_term.py
